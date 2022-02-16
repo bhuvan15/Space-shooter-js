4b24cc42621c471c8aa2905a9687d12d53434c36
bhuvan15 <bhuvanmalik321@gmail.com>
2022-02-16 17:09:30 +0530

    Creating projectile

--- script.js
+++ script.js
@@ -25,6 +25,25 @@ class Player {
     }
 }
 
+/* Projectile class */
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+    /* Canvas draw function */
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+
+}
+
 /* Setting player co-ordinates */
 const x = canvas.width / 2;
 const y = canvas.height / 2;
@@ -32,5 +51,13 @@ const y = canvas.height / 2;
 const player = new Player(x, y, 30, 'blue');
 /* Drawing player */
 player.draw();
-
 console.log(player);
+
+/* Creating a event listener on window object */
+window.addEventListener('click', (e) => {
+    /* x and y cordinates using event */
+    let x = e.clientX;
+    let y = e.clientY
+    const projectile = new Projectile(x, y, 5, 'red', null);
+    projectile.draw();
+})
